dca2fe07f77504b1b4cb069bed01332c848634dc
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2021-01-17 08:25:50 +0100

    update compose

--- setup.sh
+++ setup.sh
@@ -75,7 +75,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-docker-compose -f ${WORKSHOP_ROOT}/docker-compose.yaml up --no-recreate -d
+docker-compose -f ${WORKSHOP_ROOT}/docker-compose.yaml up -d
 
 echo "We are ready to go!!!!"
 echo "Your ip is: ${MY_IP}"
